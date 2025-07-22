ebf8dacc8540f2caf9e8b7b9d32755bf5f9a9fad
jsn <jesuino.vf@gmail.com>
2025-07-22 16:39:42 -0300

    refactor: add train_one_epoch for predictive coding

--- src/train/pc.py
+++ src/train/pc.py
@@ -1,6 +1,8 @@
 import time
 
+import sklearn.metrics
 import torch
+from torch import nn
 
 from . import T2PC
 
@@ -13,84 +15,100 @@ def predictive_coding(
     optimizer,
     n_epochs,
     device: torch.device,
-    INFERENCE_LEARNING_RATE: float,
-    N_INFERENCE_STEPS: int,
+    inference_lr: float,
+    n_inference_steps: int,
 ):
     model.to(device)
 
-    history = {
-        "train_losses": [],
-        "train_accuracies": [],
-        "val_losses": [],
-        "val_accuracies": [],
-        "epoch_times": [],
-    }
+    train_losses: list[float] = []
+    train_accuracies: list[float] = []
+    val_losses: list[float] = []
+    val_accuracies: list[float] = []
+    epoch_times: list[float] = []
 
     print("Starting predictive coding training")
     print("-" * 60)
 
     for epoch in range(n_epochs):
+        # Training phase
         t1 = time.time()
-        model.train()
-        running_loss, correct, total_samples = 0.0, 0, 0
+        train_loss, train_acc = train_one_epoch(
+            model,
+            train_loader,
+            criterion,
+            optimizer,
+            device,
+            inference_lr,
+            n_inference_steps,
+        )
+        epoch_times.append(time.time() - t1)
 
-        for x, y in train_loader:
-            x, y = x.to(device), y.to(device)
+        # Validation phase
+        val_metrics = evaluate(model, val_loader, criterion, device)
 
-            vhat, loss, _, _, _ = T2PC.PCInfer(
-                model,
-                criterion,
-                x,
-                y,
-                "Strict",
-                eta=INFERENCE_LEARNING_RATE,
-                n=N_INFERENCE_STEPS,
-            )
-
-            optimizer.step()
-            optimizer.zero_grad()
-            running_loss += loss.item()
-
-            _, preds = torch.max(vhat[-1].data, 1)
-            total_samples += y.size(0)
-            correct += (preds == y).sum().item()
-
-        history["train_losses"].append(running_loss / len(train_loader))
-        history["train_accuracies"].append(correct / total_samples)
-
-        model.eval()
-        val_loss, val_correct, val_total = 0.0, 0, 0
-        with torch.no_grad():
-            for x, y in val_loader:
-                x, y = x.to(device), y.to(device)
-
-                outputs = model(x)
-                loss = criterion(outputs, y)
-                val_loss += loss.item()
-
-                _, preds = torch.max(outputs.data, 1)
-                val_total += y.size(0)
-                val_correct += (preds == y).sum().item()
-
-        history["val_losses"].append(val_loss / len(val_loader))
-        history["val_accuracies"].append(val_correct / val_total)
-        history["epoch_times"].append(time.time() - t1)
+        train_losses.append(train_loss)
+        train_accuracies.append(train_acc)
+        val_losses.append(val_metrics["loss"])
+        val_accuracies.append(val_metrics["accuracy"])
 
         print(
             f"Epoch: [{epoch + 1:02d}/{n_epochs}]  "
-            f"Train Loss: {history['train_losses'][-1]:.4f},  "
-            f"Train Acc: {history['train_accuracies'][-1]:6.2f},  "
-            f"Val Loss: {history['val_losses'][-1]:.4f},  "
-            f"Val Acc: {history['val_accuracies'][-1]:6.2f},  "
-            f"Time: { history['epoch_times'][-1]:5.2f}s"
+            f"Train Loss: {train_loss:.4f},  "
+            f"Train Acc: {train_acc:6.2f},  "
+            f"Val Loss: {val_metrics['loss']:.4f},  "
+            f"Val Acc: {val_metrics['accuracy']:6.2f},  "
+            f"Time: {epoch_times[-1]:5.2f}s"
         )
 
-    return history
+    return {
+        "train_losses": train_losses,
+        "train_accuracies": train_accuracies,
+        "val_losses": val_losses,
+        "val_accuracies": val_accuracies,
+        "epoch_times": epoch_times,
+    }
 
 
-# TODO: train_one_epoch
-def train_one_epoch():
-    pass
+def train_one_epoch(
+    model: nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    criterion: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+    inference_lr: float,
+    n_inference_steps: int,
+) -> tuple[float, float]:
+    model.train()
+    total_loss = 0.0
+    y_pred = []
+    y_true = []
+
+    model.train()
+    for x, y in dataloader:
+        x, y = x.to(device), y.to(device)
+
+        vhat, loss, _, _, _ = T2PC.PCInfer(
+            model,
+            criterion,
+            x,
+            y,
+            "Strict",
+            eta=inference_lr,
+            n=n_inference_steps,
+        )
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        total_loss += loss.item()
+        _, preds = torch.max(vhat[-1].data, 1)
+
+        y_pred.extend(preds.cpu().numpy())
+        y_true.extend(y.cpu().numpy())
+
+    avg_loss: float = total_loss / len(dataloader)
+    accuracy: float = sklearn.metrics.accuracy_score(y_true, y_pred)
+    return avg_loss, accuracy
 
 
 def evaluate(
